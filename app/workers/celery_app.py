d6301c39c0436fec53f6c982bf0ca6c81157b846
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-26 15:43:37 -0500

    Create celery_app.py

---
+++ app/workers/celery_app.py
@@ -0,0 +1,12 @@
+from celery import Celery
+from app.core.config import settings
+
+celery_app = Celery(
+    "agent_worker",
+    broker=settings.redis_url,
+    backend=settings.redis_url,
+)
+
+celery_app.conf.task_routes = {
+    "app.workers.tasks.*": {"queue": "agent_queue"}
+}
